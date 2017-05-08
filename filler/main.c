81b61a64a0e72b09e2eaf470b56755a395f2b4aa
NataliYatsulka <yatsulka@gmail.com>
2017-05-08 13:20:26 +0300

    Worked only for first step, then timeout. Smth with gnl

--- filler/main.c
+++ filler/main.c
@@ -12,7 +12,7 @@
 
 #include "filler.h"
 
-#define WHERE_READ 3 //0=з консолі, 3=зфайлу
+#define WHERE_READ 0 //0=з консолі, 3=зфайлу
 
 int		ok_put(t_filler *f, int i, int j)
 {
@@ -20,21 +20,23 @@ int		ok_put(t_filler *f, int i, int j)
 	int		l;
 
 	k = -1;
-	f->x_return = i - 1;
-	f->y_return = j - 1;
-	while (++k < f->t_y)
+	f->x_return = i;
+	f->y_return = j;
+	while (++k < f->t_x)
 	{
 		l = 0;
-		while (l < f->t_x)
+		while (l < f->t_y)
 		{
-			f->map[i][j] = f->tkn[k][l];
-			l++;
-			j++;
+			if (f->tkn[k][l] == '.')
+				l++;
+			else
+			{
+				f->map[i + k][j + l] = f->tkn[k][l];
+				l++;
+			}
 		}
-		i++;
 	}
 	return (1);
-	
 }
 
 int		check_map_and_tkn(t_filler *f, int i, int j)
@@ -45,13 +47,11 @@ int		check_map_and_tkn(t_filler *f, int i, int j)
 
 	k = -1;
 	count = 0;
-	while (i < f->len_map_y - f->t_y && ++k < f->t_y)
+	while (++k < f->t_x)
 	{
 		l = 0;
-		while (j < f->len_map_x - f->t_x && l < f->t_x)
+		while (l < f->t_y)
 		{
-			// printf("%s\n",&(f->map)[i][j] );
-			// printf("try: %d:%d; map = %c; tkn = %c \n", i+k, j+l, f->map[i + k][j + l], f->tkn[k][l]);
 			if ((f->map[i + k][j + l] == '.' || f->map[i + k][j + l] == f->sgn) &&
 				(f->tkn[k][l] == '.' || f->tkn[k][l] == f->sgn))
 			{
@@ -62,32 +62,8 @@ int		check_map_and_tkn(t_filler *f, int i, int j)
 			else
 				return (0);
 		}
-		// i = i + f->len_map_y + 1;
 	}
-	//printf("\n");
-	// while (i < f->len_map_y - f->t_y && ++k < f->t_y)
-	// {
-	// 	l = 0;
-	// 	while (j < f->len_map_x - f->t_x && l < f->t_x)
-	// 	{
-	// 		// printf("%s\n",&(f->map)[i][j] );
-	// 		if ((f->map[i][j] == '.' || f->map[i][j] == f->sgn) &&
-	// 			(f->tkn[k][l] == '.' || f->tkn[k][l] == f->sgn))
-	// 		{
-	// 			if (f->map[i][j] == f->sgn && f->tkn[k][l] == f->sgn)
-	// 				count++;
-	// 			l++;
-	// 			j++;
-	// 		}
-	// 		else
-	// 			return (0);
-	// 	}
-	// 	i = i + f->len_map_y + 1;
-	// }
-	if (count == 1)
-		return (1);
-	else
-		return (0);		
+	return (count == 1 ? 1 : 0);	
 }
 
 void	put_tkn_on_map(t_filler *f)
@@ -101,21 +77,20 @@ void	put_tkn_on_map(t_filler *f)
 		j = 0;
 		while (j < f->len_map_x - f->t_x)
 		{
-			
-			// printf("%d:%d ", i, j);
 			if (check_map_and_tkn(f, i, j) == 1)
 			{
-				if (ok_put(f, i, j) == 1)
-				{
-					ft_printf("%d %d", f->x_return, f->y_return);
-					return ;
-				}
+				f->x_return = i;
+				f->y_return = j;
+				printf("%d %d\n", f->x_return, f->y_return);
+				return ;
+				// if (ok_put(f, i, j) == 1)
+				// {
+				// 	printf("%d %d\n", f->x_return, f->y_return);
+				// 	return ;
+				// }
 			}
 			j++;
-			// else
-			// 	;
 		}
-		// printf("\n");
 	}
 }
 
@@ -126,12 +101,6 @@ void	mas_malloc(t_filler *f, int y)
 	i = -1;
 	if (!(f->tkn = (char **)ft_memalloc((y + 1) * sizeof(char *))))
 		return ;
-	// while (++i < x)
-	// {
-	// 	f->tkn[i] = ft_strnew(y);
-	// 	if (!f->tkn[i])
-	// 		return ;
-	// }
 }
 
 void	mas_malloc_map(t_filler *f, int x, int y)
@@ -149,11 +118,6 @@ void	mas_malloc_map(t_filler *f, int x, int y)
 	}
 }
 
-// void	get_coord_tkn(t_filler *f)
-// {
-
-// }
-
 void	place_token(t_filler *f, char *line)
 {
 	int		i;
@@ -162,9 +126,11 @@ void	place_token(t_filler *f, char *line)
 	i = -1;
 	while (++i < f->t_x)
 	{
+		get_next_line(WHERE_READ, &line);
 		f->tkn[i] = ft_strdup(line);
 		ft_strdel(&line);
-		get_next_line(WHERE_READ, &line);
+		// if (i + 1 < f->t_x)
+		// 	get_next_line(WHERE_READ, &line);
 	}
 	i = -1;
 	while (++i < f->t_x)
@@ -176,6 +142,7 @@ void	place_token(t_filler *f, char *line)
 				f->tkn[i][j] = f->sgn;
 		}
 	}
+
 }
 
 void	read_token_num(t_filler *f, char *line)
@@ -191,9 +158,8 @@ void	read_token_num(t_filler *f, char *line)
 	f->t_y = ft_atoi(&line[j]);
 	ft_strdel(&line);
 	mas_malloc(f, f->t_y);
-	while (get_next_line(WHERE_READ, &line))
-		place_token(f, line);
-//	get_coord_tkn(f);
+	//while (get_next_line(WHERE_READ, &line))
+	place_token(f, line);
 }
 
 void	read_numb_max_xy(t_filler *f, char *line)
@@ -208,20 +174,6 @@ void	read_numb_max_xy(t_filler *f, char *line)
 		j++;
 	f->len_map_y = ft_atoi(&line[j]);
 }
-
-// void	put_coord_xo(t_filler *f, char *line, int i, int j)
-// {
-// 	if (line[j] == 'X')
-// 	{
-// 		f->xx = i;
-// 		f->xy = j - 3;
-// 	}
-// 	else if (line[j] == 'O')
-// 	{
-// 		f->ox = i;
-// 		f->oy = j - 3;
-// 	}
-// }
 
 char	*read_plato(t_filler *f, char *line)
 {
@@ -241,10 +193,6 @@ char	*read_plato(t_filler *f, char *line)
 		if (i < f->len_map_y)
 			f->map[i] = ft_strsub(line, 4, f->len_map_y);
 		i++;
-		// while (line[j] && line[j] != 'P' && ((line[j] >= 48 && line[j] <= 57) ||
-		// 	line[j] == '.' || line[j] == ' '))
-		// 	j++;
-		// put_coord_xo(f, line, i, j);
 		if (line[j] != 'P')
 			ft_strdel(&line);
 		else
@@ -266,10 +214,14 @@ int		main(void)
 {
 	char		*line;
 	t_filler	*f;
+	int	fd;
+	int i = -1;
+
 
 #if WHERE_READ == 3
 	open("/nfs/2016/n/nyatsulk/filler/22_04_17/n_yatsulk/test.c", O_RDONLY);
 #endif
+	fd = open("/nfs/2016/n/nyatsulk/filler/22_04_17/n_yatsulk/test2.c", O_WRONLY);
 
 	line = NULL;
 	if (!(f = (t_filler *)ft_memalloc(sizeof(t_filler))))
@@ -279,17 +231,17 @@ int		main(void)
 	if (ft_strnequ(line, "$$$ exec p1", 11))
 		f->sgn = 'O';
 	ft_strdel(&line);
-	while (get_next_line(WHERE_READ, &line))
-	{
-		line = read_plato(f, line);
-		read_token_num(f, line);
-		put_tkn_on_map(f);
-//		algo(f);
-		//write(1, "main\n", 5);
-	}
-	//ft_strdel(&line);
-	//printf("end of program\n");
-	//printf("%d %d\n", f->x_return, f->y_return);
+	
+
+	get_next_line(WHERE_READ, &line);
+	line = read_plato(f, line);
+	read_token_num(f, line);
+	put_tkn_on_map(f);
+	while (++i < f->len_map_x)
+		dprintf(fd, "%s \n", f->map[i]);
+	i = -1;
+	while (++i < f->t_x)
+		dprintf(fd, "%s %d %d/// %d %d\n", f->tkn[i], f->t_x, f->t_y, f->x_return, f->y_return);
 	return (0);
 	#if WHERE_READ == 3
 		close(3);
