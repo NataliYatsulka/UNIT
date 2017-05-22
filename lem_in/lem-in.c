8ae48311a85eb39f116391af54507f057d11db05
NataliYatsulka <yatsulka@gmail.com>
2017-05-22 13:19:47 +0300

    added func that read numb of ants

--- lem_in/lem-in.c
+++ lem_in/lem-in.c
@@ -12,11 +12,40 @@
 
 #include "lemin.h"
 
-void	write_output(t_read **read, *line)
+void	ft_error(char *s)
 {
-	t_read	*read1;
+	write(1, s, ft_strlen(s));
+	exit(1);
+}
 
-	read1 = ()
+void	write_read(t_read **read, char *line)
+{
+	t_read	*list;
+
+	if (!(list = (t_read *)malloc(sizeof(t_read))))
+		ft_error("1\n");
+	list->line = NULL;
+	list->nxt = NULL;
+	list->line = ft_strdup(line);
+	list->nxt = *read;
+}
+
+void	read_nub_ant(t_output *otp, char *line)
+{
+	int		i;
+	int		numb;
+
+	i = -1;
+	numb = -1;
+	while (line[++i])
+	{
+		numb = numb * 10 + (line[i] - '0');
+		if (numb > 2147483647 || numb < -214783648)
+			ft_error("num 2147483647 ..\n");
+	}
+	if (line[i])
+		ft_error("no numb ants, only links\n");
+	otp->ants = numb;
 }
 
 void	find_numb_ants(t_read **read, t_output *otp)
@@ -27,28 +56,33 @@ void	find_numb_ants(t_read **read, t_output *otp)
 	while (!otp->ants && get_next_line(0, &line))
 	{
 		if (!line)
-			ft_error();
+			ft_error("no line\n");
 		else if (line[0] == '#')
 		{
 			if (ft_strequ(line, "##start") || ft_strequ(line, "##end"))
-				ft_error();
-			write_output(read, line);
-
+				ft_error("no numb ants before command\n");
 		}
+		else
+		{
+			if (ft_isalpha(line[0]))
+				ft_error("no numb before\n");
+			read_nub_ant(otp, line);
+		}
+		write_read(read, line);
 		ft_strdel(&line);
 	}
 }
 
 int		main(void)
 {
-	// char		*line;
-	t_read		*r;
-	t_output	*o;
+	t_read		*read;
+	t_output	*out;
 
-	if (!(r = (t_read *)ft_memalloc(sizeof(t_read))))
-		return (1);
-	if (!(o = (t_output *)ft_memalloc(sizeof(t_output))))
-		return (1);
-	find_numb_ants(&r, o);
-	return (0);
+	if (!(read = (t_read *)ft_memalloc(sizeof(t_read))))
+		return (0);
+	if (!(out = (t_output *)ft_memalloc(sizeof(t_output))))
+		return (0);
+	find_numb_ants(&read, out);
+	find_rooms(&read, out);
+	return (1);
 }
--- lem_in/lemin.h
+++ lem_in/lemin.h
@@ -15,11 +15,22 @@
 
 # include "libft/libft.h"
 
+/*
+**	line - gnl read
+**	nxt - next
+*/
+
 typedef struct		s_read
 {
 	char			*line;
-	struct s_read	*n;
+	struct s_read	*nxt;
 }					t_read;
+
+/*
+**	x_room y_room - coordinates near the name of room	
+**	n - next
+**
+*/
 
 typedef struct		s_room
 {
@@ -30,15 +41,24 @@ typedef struct		s_room
 	struct s_room	*n;
 }					t_room;
 
+/*
+**	sgn - 
+*/
+
 typedef struct		s_link
 {
 	char			sgn;
 }					t_link;
 
+/*
+**	ants - numb in start
+**	
+*/
+
 typedef struct		s_output
 {
 	int				ants;
-	struct s_read
+	
 }					t_output;
 
 int					main(void);
