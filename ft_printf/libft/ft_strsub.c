437f1bc68833ffde540f126465581f65c5b275e6
NataliYatsulka <yatsulka@gmail.com>
2017-06-22 19:44:18 +0300

    added libft to ft_printf

---
+++ ft_printf/libft/ft_strsub.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nyatsulk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/29 21:32:52 by nyatsulk          #+#    #+#             */
+/*   Updated: 2016/11/29 21:32:53 by nyatsulk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*p;
+	size_t	i;
+
+	i = 0;
+	p = (char *)malloc(len + 1);
+	if (p == NULL || s == NULL || start > (unsigned int)ft_strlen(s))
+		return (NULL);
+	while (i < len)
+	{
+		p[i] = s[start];
+		i++;
+		start++;
+	}
+	p[i] = '\0';
+	return (p);
+}
